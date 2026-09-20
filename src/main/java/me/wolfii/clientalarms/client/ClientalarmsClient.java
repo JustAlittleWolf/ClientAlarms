4492436336ecc2a0fe97a1f3929e2b9c217addda
JustAlittleWolf <jakobi5000@gmail.com>
2026-09-20 11:17:20 +0200

    Init

---
+++ src/main/java/me/wolfii/clientalarms/client/ClientalarmsClient.java
@@ -0,0 +1,10 @@
+package me.wolfii.clientalarms.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ClientalarmsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
